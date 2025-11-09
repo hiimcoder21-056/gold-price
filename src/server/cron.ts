5092e13c22a2dd428105474b97839a5275a71970
“Hiiamcoder” <hiimcoder21@gmail.com>
2025-11-09 11:18:50 +0700

    feat: remove-log

--- src/server/cron.ts
+++ src/server/cron.ts
@@ -2,6 +2,5 @@ import { requestNewApiKey } from "@/services/apikey.service";
 import cron from "node-cron";
 
 cron.schedule("0 23 */14 * *", async () => {
-    console.log("🔁 Refreshing VNAppMob API key...");
     await requestNewApiKey();
 });
